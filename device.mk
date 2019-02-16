68a3c86eab600116ac1b9715e60476cbf6dd6b6c
Abhat27 <advaithbhat9@gmail.com>
2019-02-16 15:52:15 +0000

    kuntao:Add new Doze package

--- device.mk
+++ device.mk
@@ -152,9 +152,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qdcm_calib_data_mipi_mot_cmd_smd_1080p_549.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_mipi_mot_cmd_smd_1080p_549.xml
 
-# Doze mode
+# Doze 
 PRODUCT_PACKAGES += \
-    XiaomiDoze
+    LenovoDoze
 
 # DRM
 PRODUCT_PACKAGES += \
