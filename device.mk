b1679ab5aedd8b1a0c33693b2e034a041bde6199
Abhat27 <advaithbhat9@gmail.com>
2019-03-07 11:40:03 +0000

    kuntao: add optimised thernal configs

--- device.mk
+++ device.mk
@@ -370,9 +370,11 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
     thermal.msm8953
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
+    $(LOCAL_PATH)/configs/thermal-engine-sgame.conf:system/etc/thermal-engine-sgame.conf
 
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
