fa3e422fb870daf93c8512017fa5fe1a31c79952
Abhat27 <advaithbhat9@gmail.com>
2019-03-07 12:10:28 +0000

    kuntao: nuke TC

--- device.mk
+++ device.mk
@@ -444,3 +444,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/perf/perf-profile5.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile5.conf \
     $(LOCAL_PATH)/perf/perf-profile6.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile6.conf \
     $(LOCAL_PATH)/perf/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
